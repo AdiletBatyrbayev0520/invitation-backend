9d0faf964db3918a078092fba9d54a460fd39165
Adilet <adilet.batyrbayev0520@gmail.com>
2024-07-27 20:45:27 +0500

    fix:server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: ['http://localhost:3000', 'https://invitation-frontend-umber.vercel.app']
 }));
 app.use(bodyParser.json());
 
