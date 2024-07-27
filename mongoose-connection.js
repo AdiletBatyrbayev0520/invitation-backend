cd9b95cfbbff548ec41f4635df02447283d890b7
Adilet <adilet.batyrbayev0520@gmail.com>
2024-07-27 20:12:22 +0500

    fix:mongoose

--- mongoose-connection.js
+++ mongoose-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`${process.env.MONGO_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
 
